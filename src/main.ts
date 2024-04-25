41f59b5d37c29be2bae7033663436f73fa271201
StyleShit <evyatar.daud@gmail.com>
2024-04-25 09:27:13 +0300

    chore: cleanup

--- src/main.ts
+++ src/main.ts
@@ -11,13 +11,10 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>Signals</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
   </div>
 `;
 
